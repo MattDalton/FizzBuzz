ac71d21084d035191d166f5bcc7785dfbbc0a8c9
MattDalton <mattydalton4@gmail.com>
2023-10-19 14:58:41 +0100

    Implement changes to pass TestNotDivisibleBy3Or5

--- FizzBuzzProject/FizzBuzz.cs
+++ FizzBuzzProject/FizzBuzz.cs
@@ -13,7 +13,7 @@ namespace FizzBuzzProject
             if (number % 3 == 0 && number % 5 == 0) return "FizzBuzz";
             if (number % 3 == 0) return "Fizz";
             if (number % 5 == 0) return "Buzz";
-            return "";
+            return number.ToString();
         }
     }
 }
