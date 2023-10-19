e0611ea3d9fcbc7448e030e86873a25b1167f4c6
MattDalton <mattydalton4@gmail.com>
2023-10-19 15:03:32 +0100

    Implement main method
    Main method runs from 1 to 100 printing the output in the console

--- FizzBuzzProject/Program.cs
+++ FizzBuzzProject/Program.cs
@@ -1,7 +1,14 @@
-﻿class Program
+﻿using FizzBuzzProject;
+
+class Program
 {
     public static void Main(string[] args)
     {
+        FizzBuzz FizzBuzz = new FizzBuzz(); 
 
+        for (int i = 1; i <= 100; i++)
+        {
+            Console.WriteLine(FizzBuzz.GetOutput(i));
+        }
     }
 }
